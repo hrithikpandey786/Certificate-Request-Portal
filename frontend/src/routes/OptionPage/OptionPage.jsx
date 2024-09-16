a284b000a9d8ff5bbb2b1275ae4d3c990134c4ed
Hrithik <hrithikpandey786@gmail.com>
2024-09-16 14:00:34 +0530

    first commit

---
+++ frontend/src/routes/OptionPage/OptionPage.jsx
@@ -0,0 +1,50 @@
+import "./optionPage.scss";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthContext";
+import apiRequest from "../../../lib/apiRequest";
+
+
+export default function OptionPage(){
+    const {currentUser, updateUser} = React.useContext(AuthContext);
+    const [error, setError] = React.useState("");
+    const [isDisabled, setIsDisabled] = React.useState(false);
+    const id = currentUser.id;
+    const navigate = useNavigate();
+
+    async function handleLogout(){
+        setError("");
+        setIsDisabled(true);
+
+        try{
+            await apiRequest.post("/auth/logout");
+
+            updateUser(null);
+            navigate("/");
+        } catch(err){
+            console.log(err);
+            setError(err.response.data.message)
+        } finally{
+            setIsDisabled(false);
+        }
+    }
+
+    return(
+        <div className="optionPageContainer">
+            <div className="heading">
+            <h1>Certificate Requisition Portal</h1>
+            </div>
+            <h2>Applying/Applied for</h2>
+                <Link to={`/degreeRequest/${id}`}>
+                    <button>Degree</button>
+                </Link>
+                <Link to={`/migrationRequest/${id}`}>
+                    <button>Migration</button>
+                </Link>
+                <div className="logoutSection">
+                    <button className="logout" onClick={handleLogout}>Logout</button>
+                    {error && <span>{error}</span>}
+                </div>
+        </div>
+    )
+}
